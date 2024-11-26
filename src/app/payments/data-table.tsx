ed508017c1ca3d8dd1069f244d922f7524236acf
MD-Shahzaib <dev7.whimstech@gmail.com>
2024-11-26 21:37:10 +0500

    delete unused code except data-table-with-search

--- src/app/payments/data-table.tsx
+++ src/app/payments/data-table.tsx
@@ -170,12 +170,12 @@ export function DataTable<TData, TValue>({
                     Next
                 </Button>
             </div>
+            {/* <DataTablePagination table={table} />
+            <DataTableViewOptions table={table} /> */}
             <div className="flex-1 text-sm text-muted-foreground">
                 {table.getFilteredSelectedRowModel().rows.length} of{" "}
                 {table.getFilteredRowModel().rows.length} row(s) selected.
             </div>
-            {/* <DataTablePagination table={table} />
-            <DataTableViewOptions table={table} /> */}
         </div>
     )
 }
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,12 +1,6 @@
-// import AdvancedDataTable from "./AdvancedDataTable/AdvancedDataTable";
-// import AgGridDataTable from "./AgGridDataTable/AgGridDataTable";
-// import TableContainer from "./TableContainer/TableContainer";
 import DataTableWithSearch from "./DataTableWithSearch/DataTableWithSearch";
 
 
 export {
-    // AdvancedDataTable,
-    // AgGridDataTable,
-    // TableContainer,
     DataTableWithSearch,
 }
